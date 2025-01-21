fcc2a63298bfd9d057c6bd585256819a647a5869
Emanuel Paraschiv <40666117+emyemy89@users.noreply.github.com>
2025-01-21 17:50:04 +0100

    Update Connection.java

--- Dijkstra/dijkstra/Connection.java
+++ Dijkstra/dijkstra/Connection.java
@@ -18,46 +18,5 @@ public class Connection {
 }
 
 
-// public class Connections {
-//     private Connection[] connections; // Array to hold connections
-//     private int size; // Current number of connections
-//     private int capacity; // Capacity of the connections array
 
-//     public Connections() {
-//         this.capacity = 4; // Start with an initial capacity
-//         this.connections = new Connection[capacity];
-//         this.size = 0;
-//     }
-
-    
-//     public void add(Connection connection) {
-//         if (size == capacity) {
-//             resize(); // Resize if necessary
-//         }
-//         connections[size++] = connection; // Add
-//     }
-
-//     // Resize method
-//     private void resize() {
-//         capacity *= 2; 
-//         Connection[] newConnections = new Connection[capacity];
-//         for (int i = 0; i < size; i++) {
-//             newConnections[i] = connections[i]; // Copy 
-//         }
-//         connections = newConnections; // Point to the new array
-//     }
-
-    
-//     public Connection get(int index) {
-//         if (index < 0 || index >= size) {
-//             throw new IndexOutOfBoundsException("Index out of bounds");
-//         }
-//         return connections[index];
-//     }
-
-//     // Method to get the number of connections
-//     public int getSize() {
-//         return size; // Return current number of connections
-//     }
-// }
 
